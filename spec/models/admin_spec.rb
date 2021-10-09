40eedfcae35c8174cdb95cbc5a42922b41780121
isaacbrds <isaac.brigido@gmail.com>
2021-10-09 14:08:40 -0300

    admin sign_in and sign out

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+  it "admin is valid" do 
+    admin = Admin.new(email: "admin@example.com", password: "password" )
+
+    expect(admin.email).to eq("admin@example.com")
+    expect(admin).to be_valid
+  end
+
+  it "admin is invalid" do 
+
+    admin = Admin.new(email: "admin@example.com" )
+
+    expect(admin).to_not be_valid
+  end
+end
